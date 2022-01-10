9ca2302f0b777f195dd218c5dc1ed99f7b7a11ec
Zevs <piszczatowski.artur@gmail.copm>
2022-01-10 23:26:50 +0100

    Setup

---
+++ src/main/java/com/pjatk/wordshare/WordshareApplication.java
@@ -0,0 +1,13 @@
+package com.pjatk.wordshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordshareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordshareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pjatk/wordshare/WordshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjatk.wordshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordshareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
