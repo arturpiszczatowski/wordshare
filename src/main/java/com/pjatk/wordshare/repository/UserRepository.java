fe8a27ae94cb992024b9d7641d8825da4c2fd5f9
Zevs <piszczatowski.artur@gmail.copm>
2022-01-11 00:19:23 +0100

    Added controller, repository and entity for user

---
+++ src/main/java/com/pjatk/wordshare/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.pjatk.wordshare.repository;
+
+import com.pjatk.wordshare.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
