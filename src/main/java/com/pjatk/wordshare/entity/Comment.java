01b66489fe7928e21b8d2d5e717a890ac24a4ce5
Zevs <piszczatowski.artur@gmail.copm>
2022-01-11 14:59:23 +0100

    Comments

--- src/main/java/com/pjatk/wordshare/entity/Comment.java
+++ src/main/java/com/pjatk/wordshare/entity/Comment.java
@@ -1,4 +1,15 @@
 package com.pjatk.wordshare.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comments")
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String text;
+
 }
