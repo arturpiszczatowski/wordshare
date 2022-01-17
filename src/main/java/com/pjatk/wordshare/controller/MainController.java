17c9e1ab3ab64852db48a6e4225e925b0e580326
s20151 <s20151@pjwstk.edu.pl>
2022-01-18 00:36:44 +0100

    added working home page

--- src/main/java/com/pjatk/wordshare/controller/MainController.java
+++ src/main/java/com/pjatk/wordshare/controller/MainController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/")
-    public String homePage() {
+    public String homePage(Model model) {
         return "home";
     }
 }
