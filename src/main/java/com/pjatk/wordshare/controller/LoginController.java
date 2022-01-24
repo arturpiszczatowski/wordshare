15bc2819affa8bd494241a01af4b314ff12b0426
s20151 <s20151@pjwstk.edu.pl>
2022-01-24 20:18:02 +0100

    login.html

--- src/main/java/com/pjatk/wordshare/controller/LoginController.java
+++ src/main/java/com/pjatk/wordshare/controller/LoginController.java
@@ -3,11 +3,15 @@ package com.pjatk.wordshare.controller;
 import com.pjatk.wordshare.exception.UnauthorizedException;
 import com.pjatk.wordshare.login.LoginRequest;
 import com.pjatk.wordshare.security.AuthenticationService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
 @RequestMapping("/login")
 public class LoginController {
 
@@ -19,10 +23,13 @@ public class LoginController {
 
 
     @PostMapping()
-    public void login(LoginRequest loginRequest){
+    public String login(LoginRequest loginRequest , HttpServletResponse response, Model model){
         var isLogged = authenticationService.login(loginRequest.getUsername(),loginRequest.getPassword());
         if(!isLogged){
-            throw new UnauthorizedException();
+            response.setStatus (HttpStatus.UNAUTHORIZED.value ());
+            model.addAttribute("logError","logError");
+            return "login";
         }
+        return "home";
     }
 }
